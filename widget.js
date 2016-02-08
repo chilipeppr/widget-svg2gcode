4ed4f2af0d5a5345699cea1a3c3ddc2e7f916864
John Lauer as ChiliPeppr on Github <jlauer@chilipeppr.com>
2016-02-08 18:58:25 +0000

    Made some changes to ChiliPeppr widget using Cloud9

--- widget.js
+++ widget.js
@@ -104,7 +104,7 @@ cpdefine("inline:com-zipwhip-widget-beerbot", ["chilipeppr_ready", /* other depe
          * The ID of the widget. You must define this and make it unique.
          */
         id: "com-zipwhip-widget-beerbot", // Make the id the same as the cpdefine id
-        name: "Widget / Beer Bot", // The descriptive name of your widget.
+        name: "textabeer", // The descriptive name of your widget.
         desc: "A widget for controlling the Beer Bot.",
         url: "(auto fill by runme.js)",       // The final URL of the working widget as a single HTML file with CSS and Javascript inlined. You can let runme.js auto fill this if you are using Cloud9.
         fiddleurl: "(auto fill by runme.js)", // The edit URL. This can be auto-filled by runme.js in Cloud9 if you'd like, or just define it on your own to help people know where they can edit/fork your widget
